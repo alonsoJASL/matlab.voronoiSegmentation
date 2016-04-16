c3f6edc065744e877bcace45d4a8214e1969fc63
JOSE ALONSO SOLIS-LEMUS <Jose.Solis-Lemus@city.ac.uk>
2016-04-16 12:21:49 +0100

    Voronoizone optimised for faster running time.

--- voronoizone.m
+++ voronoizone.m
@@ -1,42 +1,37 @@
-function [voroimage,subImage] = voronoizone(x,y,img)
-% Voronoi diagram based image zoning
-%
-% [voroimage,sub_image]=voronoizone(x,y,img)
+function [voronoiImage,subImage] = voronoizone(x,y,Im)
+%           VORONOI DIAGRAM BASED IMAGE ZONING
+% 
 % For a defined number of points on an image plane, voronoizone computes
 % the voronoi diagram on the image space and divide the image into sub
 % images according to the zones.
-
-% Based on code by Kalyan S Dash, IIT Bhubaneswar, check original on: 
+%
+% USAGE:
+%           [voronoiImage,subImage] = voronoizone(x,y,Im)
 % 
+%
+% Based on code by Kalyan S Dash, IIT Bhubaneswar, check original on: 
 % <http://uk.mathworks.com/matlabcentral/fileexchange/45893-voronoi-diagram-based-image-zoning/content/voronoizone.m>
 %
 
-szImg=size(img);
-voroimage = zeros(szImg);
-dis=zeros(length(x),1);
+% Part of the matlab.vornoiSegmentation package hosted at:
+% <https://github.com/alonsoJASL/matlab.voronoiSegmentation.git>
+
+szImg=size(Im);
+voronoiImage = zeros(szImg);
 
 for i=1:szImg(1)
-    for j=1:szImg(2)
-                
-        for k=1:length(x)
-            dis(k)= (i-y(k))^2 + (j-x(k))^2;
-        end
-        
-        %[~,index]=sort(dis,'ascend');
-        
-        [~,optk]=min(dis); %index(1);
-        
-        voroimage(i,j)=optk;
-        
+    for j=1:szImg(2)              
+        dis = (i-y).^2 + (j-x).^2;
+       
+        [~,optk]=min(dis); 
+        voronoiImage(i,j)=optk(1);
     end
 end
 
 if nargout > 1
     subImage = cell([length(x) 1]);
     for k=1:length(x)
-        
-        subImage{k}=img(find(voroimage==optk));
-        
+        subImage{k}=Im(voronoiImage==optk(1));
     end
     
 end
