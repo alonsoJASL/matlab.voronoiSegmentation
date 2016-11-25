28efeb1f77f2e8ca68cae9aeb68a116f66a7fb35
Jose Alonso Solis-Lemus <alonso.jasl@gmail.com>
2016-11-25 14:42:12 +0000

    Documentation!

--- simpleClumpsSegmentation.m
+++ simpleClumpsSegmentation.m
@@ -1,6 +1,25 @@
 function [simpleRed, simpleGreen] = simpleClumpsSegmentation(X)
-%
-%
+%           SIMPLE CLUMPS SEGMENTATION
+% A simple hysteresis thresholding method based on Otsu's threshold. 
+% If size(X,3)=3, the program returns a segmentation of X(:,:,1) as well as
+% X(:,:,2). If size(X,3)=1, then the program returns the segmentation of X. 
+% On both cases, it filters the imagesc with a Gaussian filter of size 5.
+% It also performs a morphological filling of any holes, followed by an
+% open operation.
+% 
+% USAGE: [simpleRed, simpleGreen] = simpleClumpsSegmentation(X)
+%                    [simpleSegm] = simpleClumpsSegmentation(X)
+% 
+% INPUT:
+%                       X := matrix of size(X)=[M,N,3] or [M,N]
+% OUTPUT:
+%               simpleRed := segmentation of X(:,:,1)
+%             simpleGreen := segmentation of X(:,:,2)
+% 
+%              simpleSegm := segmentation of X, when X is grey-scale.
+% 
+% 
+% 
 
 if size(X,3) == 1
     green = X;
@@ -26,7 +45,8 @@ else
     red = X(:,:,1);
     green = X(:,:,2);
     
-    f = fspecial('gaussian',[5 5],1);
+    f = fspecial('gaussian',[7 7],1);
+    %f = ones(5)./5;
     
     filtRed = imfilter(red,f);
     filtGreen = imfilter(green,f);
